1bebf038d7b221a262c489f256aaa721d2ccf9b8
Robert Musungu <robertmusungu3@gmail.com>
2021-07-09 04:19:46 +0300

    starting up a flask-app server

---
+++ starting_flaskapp.py
@@ -0,0 +1,21 @@
+'''
+Starting flask app
+'''
+from flask import Flask
+import os
+import time
+import sys
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Hello World"
+
+if __name__ == "__main__":
+    localhost = "127.0.0.1"
+    app = app.run(
+            debug=True,
+            host=localhost,
+            port=5002
+            )
